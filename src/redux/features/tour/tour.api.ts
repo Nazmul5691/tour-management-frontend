136ad390697d5205bfdb1b27fe711e6a30968b15
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-08-20 23:37:01 +0600

    implementing-pagination-with-previous-and-next-buttons

--- src/redux/features/tour/tour.api.ts
+++ src/redux/features/tour/tour.api.ts
@@ -37,9 +37,10 @@ export const tourApi = baseApi.injectEndpoints({
 
 
         getTourTypes: builder.query({
-            query: () => ({
+            query: (params) => ({
                 url: "/tour/tour-types",
                 method: "GET",
+                params: params,
             }),
             providesTags: ["TOUR"],
             // transformResponse: (response) => response.data       //j part tuku dorker sudhu sei tuku nibo 
